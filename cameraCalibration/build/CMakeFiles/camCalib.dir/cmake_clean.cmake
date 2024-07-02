ec408d7d597c15b51bd11f02b967461f094825e7
YUMIAO HU <2663221H@STUDENT.GLA.AC.UK>
2024-07-02 12:24:54 +0100

    calibrate the camera with chessboard

---
+++ cameraCalibration/build/CMakeFiles/camCalib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camCalib.dir/main.cpp.o"
+  "CMakeFiles/camCalib.dir/main.cpp.o.d"
+  "camCalib"
+  "camCalib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camCalib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
